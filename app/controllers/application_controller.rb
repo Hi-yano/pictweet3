cd380603a3e8bfb7b1165d4c0c5e23d0317f2b5b
Hi-yano <begin_2020@icloud.com>
2021-09-25 21:23:15 +0900

    application_controller.rbの編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
+
+    # ifオプション = 値にメソッド名にすることで、その戻り値がtrueであった場合のみに処理を行う。
+    # 今回は :devise_controller? というdeviseのヘルパーメソッド名を指定してdeviseに関するコントローラの場合のみ処理を実行する。
+
+    # devise_parameter_sanitizerメソッド = deviseにおけるparamsのようなメソッド。
+    # ログイン、新規登録などのリクエストからパラメータを取得できる。
+    # permitメソッドを組み合わせることで、deviseで定義されているストロングパラメータに対し、自分で新しく追加したカラムも指定して組み合わせることができる。
