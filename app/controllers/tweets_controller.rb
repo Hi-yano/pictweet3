8a79574a3169eb369f9fc0f1bf53f8378899d951
Hi-yano <begin_2020@icloud.com>
2021-09-26 16:28:19 +0900

    mergeメソッドの追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -32,11 +32,14 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text)
+    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
     # railsのアプリケーションにおいてデータの保存や変更を行う際には、ストロングパラメータを用いることが定石。
     # ストロングパラメータ → params.require().permit()で、特定のキーを受け取るように制限。
     # requireメソッドは、どの情報を取得するか選択する。ストロングパラメーターとして利用する場合は、主にモデル名を指定する。
     # permitメソッドは、取得したいキーを指定でき、指定したキーと値のセットのみを取得する。
+
+    # current_userメソッドは現在ログインしているユーザの情報を取得できる。deviseを導入している為使用できるメソッド。
+    # current_userのidをmergeメソッドで統合している。mergeメソッドはハッシュを統合させるためのRubyのメソッド。
   end
 
   def set_tweet
