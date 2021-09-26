3d9491e3fbcb2de00698c6346be1cc120623c955
Hi-yano <begin_2020@icloud.com>
2021-09-26 16:39:35 +0900

    users_controller.rbのshowアクションの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tweets#index'
   resources :tweets       #, only: [:index, :new, :create, :destroy, :edit, :update, :show]   # 7つのアクションを記述した場合はonlyオプションは不要になる。
+  resources :users, only: :show   # これで、users_controller.rbのshowアクションを実行するルーティングを設定した
 end
