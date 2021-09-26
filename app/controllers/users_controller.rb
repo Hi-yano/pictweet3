b08b496979247d0489c95cdfb4a7674a0e7a385a
Hi-yano <begin_2020@icloud.com>
2021-09-26 22:01:20 +0900

    usersコントローラの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @tweets = current_user.tweets
+    user = User.find(params[:id]) # 送られてきたidをparamsで取得。findメソッドで探し、変数userに代入
+    @nickname = user.nickname     # それぞれインスタンス変数に格納 
+    @tweets = user.tweets
   end
 end
