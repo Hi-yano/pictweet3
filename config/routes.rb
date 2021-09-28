06a4ede626022ff6a2476d23ee4a8240fc558a78
Hi-yano <begin_2020@icloud.com>
2021-09-28 23:26:34 +0900

    collectionの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
   resources :tweets do  #, only: [:index, :new, :create, :destroy, :edit, :update, :show]   # 7つのアクションを記述した場合はonlyオプションは不要になる。
     resources :comments, only: :create  # ルーティングにネストを使用しなければ、モデルと結びついている別モデルのid情報が送れなくなる。ルーティングをネストさせる一番の理由は、レコードのidをparamsに追加してコントローラに送るため。
+    collection do   # collectionを使用すると、生成されるルーティングのURLと実行されるコントローラを任意にカスタムできる。（似たものにmemberがある）
+      get 'search'
+    end
   end
   resources :users, only: :show   # users_controller.rbのshowアクションを実行するルーティングを設定した
 end
