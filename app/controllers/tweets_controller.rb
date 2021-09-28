9e9b0fe9c25be3226bec2c350e5ea8136000b52c
Hi-yano <begin_2020@icloud.com>
2021-09-28 22:05:28 +0900

    表示順の変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @tweets = Tweet.includes(:user)    # 「@」月のものはインスタンス変数。インスタンス変数の値は、コントローラとビューで共有される。
+    @tweets = Tweet.includes(:user).order("created_at DESC")    # 「@」月のものはインスタンス変数。インスタンス変数の値は、コントローラとビューで共有される。
     # includesメソッドを使うとすべてのレコードを取得する。N+1問題回避のため。
   end
 
